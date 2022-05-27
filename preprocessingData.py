2cae4f88a23a964c8e1e307809b70cb01eb65e7d
Daka183 <89138824+Daka183@users.noreply.github.com>
2022-05-27 15:22:03 +0300

    Update preprocessingData.py

--- preprocessingData.py
+++ preprocessingData.py
@@ -7,17 +7,19 @@ class PreprocessingData():
     """Предварительная обработка сырых данных"""
 
     def __init__(self):
-        self.exel_data_df = None
-        self.size_exel_data_df = None
-        self.all_category = []
-        self.quantity_category = -1
-        self.result_dict = {}
+        self.exel_data_df = None        # Сырые данные
+        self.size_exel_data_df = None   # Размер сырых данных
+        self.all_category = []          # Массив категорий
+        self.quantity_category = -1     # Количество категорий
+        self.result_dict = {}           # Обработанные данные
 
+    # Загрузка сырых данных
     def load_data(self):
         self.exel_data_df = pandas.read_excel(settings.path_data_base, sheet_name=settings.sheet_name)
         self.size_exel_data_df = len(self.exel_data_df)
         #self.size_exel_data_df = 1000
 
+    # Функция для отчистки текста
     def clean_text(self, text):
         text = text.lower()
         text = re.sub('\-\s\r\n\s{1,}|\-\s\r\n|\r\n', '', text) # Редактирование строки
@@ -31,6 +33,7 @@ class PreprocessingData():
 
         return text
     
+    # Нахождение всех категорий
     def category_definition(self):
         self.all_category.append(self.exel_data_df.values[0][3])
 
@@ -55,12 +58,14 @@ class PreprocessingData():
         
         self.quantity_category = len(self.all_category)
     
+    # Сохранение списка категорийй
     def save_all_categories(self, path_file):
         all_cat_dict = {}
         all_cat_dict['Категории'] = self.all_category
         data_frame = pandas.DataFrame(all_cat_dict)
         data_frame.to_excel(path_file)
     
+    # Поиск конкретной категории
     def find_category(self, text):
         i = 0
 
@@ -70,6 +75,7 @@ class PreprocessingData():
             i = i + 1
         return -1
 
+    # Формирование итогового дата фрейма
     def preparation_data_frame(self):
         request = []
         caregory = []
@@ -86,14 +92,17 @@ class PreprocessingData():
         self.result_dict['Запрос'] = request
         self.result_dict['Категория'] = caregory
 
+    # Сохранение дата фрейма
     def save_data_frame(self, path_file):
         data_frame = pandas.DataFrame(self.result_dict)
         data_frame.to_excel(path_file)
     
+    # Загрузка списка категорий
     def load_caregories(self):
         cat_df = pandas.read_excel(settings.path_all_category, sheet_name=settings.sheet_name_categories)
         return cat_df
     
+    # Определение максимальной длинны слов в запросе
     def max_word(self):
         max_words = 0
         data_df = pandas.read_excel(settings.path_data_preprocessing, sheet_name=settings.sheet_name_preprocessing)
